36bfb960d7e830c6ecc1f5231dea293e82578d4f
chandrakala24vm <chandrakalavm24@gmail.com>
2025-04-23 13:24:04 +0530

    Delete terraform-eks-main/backend.tf
    unable to edit

--- terraform-eks-main/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "s3-bucket-backend"
-    key            = "eks-terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraformstatelock"
-    encrypt        = true
-  }
-}
