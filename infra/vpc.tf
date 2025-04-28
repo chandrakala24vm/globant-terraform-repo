f822b662a20fec0dae61a520413d9a88defb0633
chandrakala24vm <chandrakala24vm@gamil.com>
2025-04-28 10:28:40 +0530

    adding existing vpc

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -14,35 +14,53 @@ resource "random_string" "suffix" {
   special = false
 }
 
+# module "vpc" {
+#   source  = "terraform-aws-modules/vpc/aws"
+#   version = "5.7.0"
+
+#   name                 = "chandrakala-eks-vpc"
+#   cidr                 = var.vpc_cidr
+#   azs                  = data.aws_availability_zones.available.names
+#   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
+#   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24"]
+#   enable_nat_gateway   = true
+#   single_nat_gateway   = true
+#   enable_dns_hostnames = true
+#   enable_dns_support   = true
+
+#   tags = {
+#     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+#   }
+
+#   public_subnet_tags = {
+#     "kubernetes.io/cluster/${local.cluster_name}"  = "shared"
+#     # "kubernetes.io/role/elb"                       = "1"
+#     "k8s.io/cluster-autoscaler/enabled"             = "true"
+#     "k8s.io/cluster-autoscaler/${local.cluster_name}" = "owned"
+#   }
+
+#   private_subnet_tags = {
+#     "kubernetes.io/cluster/${local.cluster_name}"   = "shared"
+#     # "kubernetes.io/role/internal-elb"               = "1"
+#     "k8s.io/cluster-autoscaler/enabled"             = "true"
+#     "k8s.io/cluster-autoscaler/${local.cluster_name}" = "owned"
+#   }
+# }
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.7.0"
 
-  name                 = "chandrakala-eks-vpc"
-  cidr                 = var.vpc_cidr
-  azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  name = "default-vpc"
+  cidr = data.aws_vpc.default.cidr_block
+  azs  = data.aws_availability_zones.available.names
+
+  public_subnets = data.aws_subnets.default_public.ids
+
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-  }
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}"  = "shared"
-    # "kubernetes.io/role/elb"                       = "1"
-    "k8s.io/cluster-autoscaler/enabled"             = "true"
-    "k8s.io/cluster-autoscaler/${local.cluster_name}" = "owned"
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}"   = "shared"
-    # "kubernetes.io/role/internal-elb"               = "1"
-    "k8s.io/cluster-autoscaler/enabled"             = "true"
-    "k8s.io/cluster-autoscaler/${local.cluster_name}" = "owned"
   }
 }
