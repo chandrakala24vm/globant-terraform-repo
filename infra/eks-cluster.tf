584304d2ea0342ebfe1c398d39d51707e407bc00
chandrakala24vm <chandrakalavm24@gmail.com>
2025-05-06 01:44:17 +0530

    Update eks-cluster.tf

--- infra/eks-cluster.tf
+++ infra/eks-cluster.tf
@@ -1,21 +1,20 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "20.8.4"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
+
   cluster_name    = local.cluster_name
   cluster_version = var.kubernetes_version
-  subnet_ids      = [var.public_subnet_az1_id, var.public_subnet_az2_id]
   vpc_id          = module.vpc.vpc_id
+  subnet_ids      = [var.public_subnet_az1_id, var.public_subnet_az2_id]
+
+  enable_irsa                         = true
+  create_cloudwatch_log_group         = true
+  enable_cluster_creator_admin_permissions = true
+
+  cluster_endpoint_public_access         = true
+  cluster_endpoint_public_access_cidrs   = ["0.0.0.0/0"] # Replace with your IP range for security
 
   authentication_mode = "API_AND_CONFIG_MAP"
-
-  enable_irsa                     = true
-  create_cloudwatch_log_group     = true
-  enable_cluster_creator_admin_permissions = true
-  cluster_endpoint_public_access  = true
-
-  tags = {
-    cluster = "Globant"
-  }
 
   cluster_addons = {
     aws-ebs-csi-driver = {}
@@ -37,9 +36,9 @@ module "eks" {
         role = "worker"
       }
       additional_tags = {
-        nodegroup-role                                  = "worker"
-        "k8s.io/cluster-autoscaler/enabled"             = "true"
-        "k8s.io/cluster-autoscaler/${local.cluster_name}" = "owned"
+        nodegroup-role                                      = "worker"
+        "k8s.io/cluster-autoscaler/enabled"                 = "true"
+        "k8s.io/cluster-autoscaler/${local.cluster_name}"   = "owned"
       }
       update_config = {
         max_unavailable_percentage = 50
@@ -48,9 +47,15 @@ module "eks" {
   }
 
   access_entries = {
-    terraform-demo = {
-      principal_arn     = "arn:aws:iam::500662818810:user/terraform-demo"
+    ec2-connect = {
+      principal_arn     = "arn:aws:iam::500662818810:role/ec2-connect"
       kubernetes_groups = ["system:masters"]
+      type              = "STANDARD"
+      username          = "ec2-connect"
     }
+  }
+
+  tags = {
+    cluster = "Globant"
   }
 }
